24cc2048bfd9fb841db84df146a540cf3c105419
Cesar Poumian <cepoumian@Cesars-MacBook-Pro.local>
2021-07-03 12:44:45 -0600

    Add real-time search functionality with matched posts preview

--- components/Layout.js
+++ components/Layout.js
@@ -1,18 +1,19 @@
 import Head from 'next/head'
 import Header from './Header'
+import Search from './Search'
 
 export default function Layout({ title, keywords, description, children }) {
   return (
     <div>
       <Head>
         <title>{title}</title>
-        <meta name="keywords" content={keywords} />
-        <meta name="description" content={description} />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='keywords' content={keywords} />
+        <meta name='description' content={description} />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
-
       <Header />
-      <main className="container mx-auto my-7">{children}</main>
+      <Search />
+      <main className='container mx-auto my-7'>{children}</main>
     </div>
   )
 }
